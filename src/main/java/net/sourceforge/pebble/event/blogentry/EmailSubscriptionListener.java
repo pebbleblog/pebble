ea94fac8de47574300680ad4af63c3d044e13717
James Roper <james@jazzy.id.au>
2011-03-06 07:02:16 +0000

    Made email subscription listener make 'Blog entry' a link to the blog entry

--- src/main/java/net/sourceforge/pebble/event/blogentry/EmailSubscriptionListener.java
+++ src/main/java/net/sourceforge/pebble/event/blogentry/EmailSubscriptionListener.java
@@ -77,7 +77,7 @@ public class EmailSubscriptionListener extends BlogEntryListenerSupport {
 
     String subject = MailUtils.getBlogEntryPrefix(blog) + " " + blogEntry.getTitle();
 
-    String message = "Blog entry posted by " + (blogEntry.getUser() != null ? blogEntry.getUser().getName() : blogEntry.getAuthor()) + " on " + sdf.format(blogEntry.getDate());
+    String message = "<a href=\"" + blogEntry.getLocalPermalink() + "\">Blog entry</a> posted by " + (blogEntry.getUser() != null ? blogEntry.getUser().getName() : blogEntry.getAuthor()) + " on " + sdf.format(blogEntry.getDate());
     message += "\n<br>";
     if (blogEntry.getExcerpt() != null && blogEntry.getExcerpt().trim().length() > 0) {
       message += blogEntry.getExcerpt();
