41b3f390513644bf197dca0a2628680f955e9e91
James Roper <james@jazzy.id.au>
2011-05-03 13:20:17 +0000

    Added plugin views for plugins to include JSPs in their jar files

---
+++ src/main/java/net/sourceforge/pebble/web/view/PluginHtmlView.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2003-2011, Simon Brown
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   - Neither the name of Pebble nor the names of its contributors may
+ *     be used to endorse or promote products derived from this software
+ *     without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.sourceforge.pebble.web.view;
+
+/**
+ * HTML view for plugins that have JSPs in their jars
+ *
+ * @author James Roper
+ */
+public abstract class PluginHtmlView extends HtmlView {
+
+  /**
+   * Get the JSP file from the classpath
+   *
+   * @return The path to the JSP file, relative to the classpath root
+   */
+  public abstract String getClasspathJsp();
+
+  @Override
+  public void prepare() {
+    super.prepare();
+    PluginJspMaintainer.checkJsp(getClasspathJsp(), getClass(), getServletContext());
+  }
+
+  @Override
+  public final String getUri() {
+    return "/WEB-INF/" + PluginJspMaintainer.PLUGINS_JSP + "/" + getClasspathJsp();
+  }
+}
